605446e10239eb83f5ba0b98606ee6637a547bdb
sumi <ninzin60en.dev@gmail.com>
2021-03-21 17:02:52 +0900

    Initial Commit

---
+++ practice-2/practice_2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice_2App.swift
+//  practice-2
+//
+//  Created by taisei sumiyama on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct practice_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
